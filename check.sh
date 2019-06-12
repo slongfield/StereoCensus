bb52b1c55979f69aa488649589a649b30716f320
Stephen Longfield <stephen.longfield@gmail.com>
2019-06-11 22:35:55 -0700

    Set up virtualenv and upgrade from ancient version of PIL.
    fixes #1

--- check.sh
+++ check.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # check.sh
-# 
+#
 # Runs all of the tests.
 # Creates some example output in verilog/stereo/generated/gen.png
 
 pushd verilog
+
+# Set up the Python environment.
+make venv
+source venv/bin/activate
+
 # Library of simple primitives.
 pushd lib
 make clean
 make check
 popd
-# Library of stereo-census related primatives.
+# Library of stereo-census related primitives.
 pushd census
 make clean
 make check
@@ -24,5 +29,6 @@ pushd stereo
 make clean
 make check
 popd
+deactivate
+make clean
 popd
-
